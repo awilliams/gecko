9ad3ef0b5622076e50c146814030cfe01708bff9
Micah Wedemeyer <me@micahwedemeyer.com>
2014-01-26 10:11:27 -0500

    Add more options to number-secondary widget

--- lib/gecko/widget/number_secondary_stat.rb
+++ lib/gecko/widget/number_secondary_stat.rb
@@ -1,17 +1,22 @@
 module Gecko
   class Widget
     class NumberSecondaryStat < Widget
-      attr_accessor :primary_text, :primary_value, :secondary_text, :secondary_value
+      attr_accessor :primary_text, :primary_value, :primary_prefix, :secondary_text, :secondary_value, :secondary_prefix, :absolute, :type
 
       def data_payload
         {:item =>
           [{
            :text => self.primary_text,
-           :value => self.primary_value
+           :value => self.primary_value,
+           :prefix => self.primary_prefix
          }, {
            :text => self.secondary_text,
-           :value => self.secondary_value
-         }]}
+           :value => self.secondary_value,
+           :prefix => self.secondary_prefix
+         }],
+         :absolute => self.absolute,
+         :type => self.type
+        }
       end
     end
   end
